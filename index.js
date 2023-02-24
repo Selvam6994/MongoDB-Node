d24fd1f5cff0dde7673126af86a5b5bf72a2faeb
Selvam6994 <selvamyoor@gmail.com>
2023-02-24 11:13:06 +0530

    organised code

--- index.js
+++ index.js
@@ -1,75 +1,29 @@
 // const express = require("express");
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 import express from "express";
 import { MongoClient } from "mongodb";
+import movieRouter from './router/movies.js';
+import cors from "cors";
+
 const app = express();
 
 const PORT = process.env.PORT;
 console.log(process.env.PORT);
 //const MONGO_URL = "mongodb://127.0.0.1";
-const MONGO_URL= process.env.MONGO_URL;
-const client = new MongoClient(MONGO_URL); // dial
+const MONGO_URL = process.env.MONGO_URL;
+export const client = new MongoClient(MONGO_URL); // dial
 // Top level await
 await client.connect(); // call
 console.log("Mongo is connected !!!  ");
 //apply middleware globally.
+app.use(cors());
 app.use(express.json());
 
 app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
-  
 });
 
-
-app.get("/movies",async function (request, response) {
-  const movies = await client
-  .db("mongodb")
-  .collection("movies")
-  .find({}).toArray();
-  response.send(movies);
-});
-
-app.get("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  const movie = await client
-    .db("mongodb")
-    .collection("movies")
-    .findOne({ id: id });
-  // response.send(movie);
-  movie ? response.send(movie) : response.status(404).send("no match found");
-});
-
-//express.json() - middleware
-app.post("/movies",  async function (request, response) {
-  const data = request.body;
-  console.log(data);
-  const result = await client
-    .db("mongodb")
-    .collection("movies")
-    .insertMany(data);
-  response.send(result);
-});
-
-app.delete("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  const result = await client
-    .db("mongodb")
-    .collection("movies")
-    .deleteMany({ id: id });
-  result.deletedCount>=1? response.send({message:"Deleted Successfully"}) : response.status(404).send("no match found");
-});
-
-app.put("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  const data = request.body;
-  const update_result = await client
-    .db("mongodb")
-    .collection("movies")
-    .updateOne({ id: id },{$set:data});
-  // response.send(movie);
-  update_result ? response.send(update_result) : response.status(404).send("no match found");
-});
-
+app.use('/movies',movieRouter);
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
---
+++ router/movies.js
@@ -0,0 +1,61 @@
+import express from "express";
+import {client} from "../index.js"
+const  router = express.Router();
+
+  
+  router.get("/", async function (request, response) {
+
+    const movies = await client
+      .db("mongodb")
+      .collection("movies")
+      .find({})
+      .toArray();
+    response.send(movies);
+  });
+  //Mock api
+  router.get("/:id", async function (request, response) {
+    const { id } = request.params;
+    const movie = await client
+      .db("mongodb")
+      .collection("movies")
+      .findOne({ id: id });
+    // response.send(movie);
+    movie ? response.send(movie) : response.status(404).send("no match found");
+  });
+  
+  //express.json() - middleware
+  router.post("/", async function (request, response) {
+    const data = request.body;
+    console.log(data);
+    const result = await client
+      .db("mongodb")
+      .collection("movies")
+      .insertMany(data);
+    response.send(result);
+  });
+  
+  router.delete("/:id", async function (request, response) {
+    const { id } = request.params;
+    const result = await client
+      .db("mongodb")
+      .collection("movies")
+      .deleteMany({ id: id });
+    result.deletedCount >= 1
+      ? response.send({ message: "Deleted Successfully" })
+      : response.status(404).send("no match found");
+  });
+  
+  router.put("/:id", async function (request, response) {
+    const { id } = request.params;
+    const data = request.body;
+    const update_result = await client
+      .db("mongodb")
+      .collection("movies")
+      .updateOne({ id: id }, { $set: data });
+    // response.send(movie);
+    update_result
+      ? response.send(update_result)
+      : response.status(404).send("no match found");
+  });
+
+  export default router;
