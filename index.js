35ee15ac3829b44cd2fdacfa46f62aa802b5c2ae
Selvam6994 <selvamyoor@gmail.com>
2023-02-26 10:24:18 +0530

    delete by _id

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import cors from "cors";
 const app = express();
 
 const PORT = process.env.PORT;
-console.log(process.env.PORT);
 //const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env.MONGO_URL;
 export const client = new MongoClient(MONGO_URL); // dial
--- router/movies.js
+++ router/movies.js
@@ -34,12 +34,12 @@ const movies = await client
     response.send(result);
   });
   
-  router.delete("/:id", async function (request, response) {
+  router.delete("/:_id", async function (request, response) {
     const { id } = request.params;
     const result = await client
       .db("mongodb")
       .collection("movies")
-      .deleteMany({ id: id });
+      .deleteMany({ _id: id });
     result.deletedCount >= 1
       ? response.send({ message: "Deleted Successfully" })
       : response.status(404).send("no match found");
